9b5e82d7162e8aefe323ae78f5d75a911833e8b3
Alex <alejandro.rodriguezh1879@gmail.com>
2020-12-19 15:29:41 -0600

    commit v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,10 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { Camera } from '@ionic-native/camera/ngx';
 
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from 'src/environments/environment';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
