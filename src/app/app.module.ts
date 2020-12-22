34278a81b7f72a9adf8f514960337524395bfff7
Alex <alejandro.rodriguezh1879@gmail.com>
2020-12-21 22:19:54 -0600

    commit v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,11 +13,15 @@ import { Camera } from '@ionic-native/camera/ngx';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFireModule } from '@angular/fire';
 import { environment } from 'src/environments/environment';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    AngularFirestoreModule],
   providers: [
     StatusBar,
     SplashScreen,
